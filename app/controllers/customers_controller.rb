87f75fb175b855af0276269cc24d5e4be0b5ec12
Daire Toner <dtoner522@gmail.com>
2021-04-29 11:53:30 +0200

    customer form added

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,21 @@
+class CustomersController < ApplicationController
+    def new
+      @order = Order.find(params[:order_id])
+      @customer = Customer.new
+    end
+
+    def create
+      @customer = Customer.new(customer_params)
+      #@order = Order.find(params[:order_id])
+      @customer.order = @order
+      #@customer.user = current_user
+      @customer.save
+      raise
+      redirect_to root_path
+    end
+
+    def customer_params
+      params.require(:customer).permit(:first_name, :last_name, :email, :phone_number, :address_line_1, :address_line_2, :postcode, :order_id)
+    end
+
+end
