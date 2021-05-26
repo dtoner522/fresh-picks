8fb34cbddc2b8f8ccf0e611813b50d5ca363ee6f
Poppy Jennings <popjen@hotmail.com>
2021-05-26 18:07:24 +0200

    extraveg on homepage

--- app/models/extraveg.rb
+++ app/models/extraveg.rb
@@ -2,4 +2,11 @@ class Extraveg < ApplicationRecord
   monetize :price_cents
   # has_one_attached :photo
   validates :category, inclusion: { in: ["A", "B", "C"] }
+
+  def self.empty?
+    total = Extraveg.all.count
+    empty_amount = Extraveg.where(quantity:0).count
+    return total == empty_amount
+  end
 end
+
